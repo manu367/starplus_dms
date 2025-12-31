642a4a2b175bf82fcc72d104c0fae9f2a3dfa4fe
ravi <ravi0201@gmail.com>
2025-12-31 13:26:24 +0530

    graph

--- practice/Baisc.php
+++ practice/Baisc.php
@@ -1,9 +1,6 @@
 <?php
-file_put_contents("proof.txt", "Started at ".date("H:i:s").PHP_EOL, FILE_APPEND);
+$page = $_GET['num'] ?? '';
+$role = $_GET['role'] ?? '';
 
-for($i=0;$i<100;$i++){
-    file_put_contents("proof.txt", "helo\n", FILE_APPEND);
-}
-
-file_put_contents("proof.txt", "Finished\n\n", FILE_APPEND);
+echo "<h3>Page: $page</h3>";
 ?>
